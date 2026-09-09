bdd7c4e2be157542878fb776360e1913cfcf3f30
Erick <erick@erickmonteiro.com.br>
2026-09-09 15:14:24 -0300

    fix: requisição para api

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,13 +1,18 @@
-const API_URL = '/api'
+const API_URL = "/api"
 
 export const bookService = {
-  async getBooks(page, pageSize) {
-    const response = await fetch(
-      `${API_URL}/BookControllerr?page=${page}&pageSize=${pageSize}`
-    )
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    return response.json()
-  }
-} 
+	async getBooks(page, pageSize) {
+		const response = await fetch(
+			`${API_URL}/books?page=${page}&pageSize=${pageSize}`
+		)
+
+		if( !response.ok )
+		{
+			throw new Error(`HTTP error! status: ${response.status}`)
+		}
+
+		const result = await response.json()
+
+		return result.data;
+	}
+}
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,22 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://localhost:7228',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  publicDir: 'public'
-}) 
+	plugins  : [vue()],
+	resolve  : {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url))
+		}
+	},
+	server   : {
+		proxy: {
+			"/api": {
+				target      : "http://localhost:5015",
+				changeOrigin: true,
+				secure      : false,
+				//rewrite     : (path) => path.replace(/^\/api/, "")
+				/*
+				configure: (proxy, options) => {
+					proxy.on('proxyReq', (proxyReq, req, res) => {
+						console.log(`proxyReq: ${req.method} ${req.url} -> ${options.target}${proxyReq.path}`)
+					})
+					proxy.on('proxyRes', (proxyRes, req, res) => {
+						console.log(`proxyRes: ${proxyRes.statusCode} de ${req.url}`)
+					})
+					proxy.on('error', (err, req, res) => {
+						console.error(`error: ${err.message}`)
+					})
+				}
+				*/
+			}
+		}
+	},
+	publicDir: "public"
+})
